f5f5ffcf24d0e5347b8c72f762d59142d4381363
Dennis Henning <>
2022-01-05 12:25:26 +0100

    Calculated overall TRI Components

--- 06_Technology_Readiness_Index.R
+++ 06_Technology_Readiness_Index.R
@@ -20,16 +20,45 @@ load("./01_Input/00_clean_data_field.RData")
 
 ########################################## Extracting TRI 2.0 Components ##########################################################
 
-# Extracting relevant questions for TRI components
+# Extracting relevant questions for TRI subcomponents
 tri_comp <- quest_clean[, .(OPT2 = v_108, OPT4 = v_109, INN1 = v_207, INN2 = v_208, INN4 = v_209, 
                             DIS2 = v_228, DIS3 = v_229, INS1 = v_230, INS2 = v_231, INS4 = v_232)]
 
 tri_comp
 
-tri_comp[, .(Mean_OPT2 = round(mean(OPT2, na.rm = T), 2),
-             Mean_OPT4 = round(mean(OPT4, na.rm = T), 2))]
+# Mean in each SUBcomponent
+means_tri_subcomp <- tri_comp[, .(Mean_OPT2 = round(mean(OPT2, na.rm = T), 2),
+                                  Mean_OPT4 = round(mean(OPT4, na.rm = T), 2), 
+                                  Mean_INN1 = round(mean(INN1, na.rm = T), 2),
+                                  Mean_INN2 = round(mean(INN2, na.rm = T), 2),
+                                  Mean_INN4 = round(mean(INN4, na.rm = T), 2),
+                                  Mean_DIS2 = round(mean(DIS2, na.rm = T), 2),
+                                  Mean_DIS3 = round(mean(DIS3, na.rm = T), 2), 
+                                  Mean_INS1 = round(mean(INS1, na.rm = T), 2),
+                                  Mean_INS2 = round(mean(INS2, na.rm = T), 2),
+                                  Mean_INS4 = round(mean(INS4, na.rm = T), 2)
+                                   )]
+means_tri_subcomp
+
+# Mean in each TRI component
+means_tri_subcomp <- melt(means_tri_subcomp, variable.name = "Component", value.name = "Mean")
+means_tri_subcomp
+
+opt_mean <- means_tri_subcomp[ 1:2, .("Optimism (OPT)" = round(mean(Mean),2))]
+inn_mean <- means_tri_subcomp[ 3:5, .("Innovativeness (INN)" = round(mean(Mean),2))]
+dis_mean <- means_tri_subcomp[ 6:7, .("Discomfort (DIS)" = round(mean(Mean), 2))]
+ins_mean <- means_tri_subcomp[ 8:10,.("Insecurity (INS)" = round(mean(Mean), 2))]
+
+tri_comp_mean <- cbind(opt_mean, inn_mean, dis_mean, ins_mean)
+tri_comp_mean <- melt(tri_comp_mean, variable.name = "TR Components", value.name = "Mean")
+
+tri_comp_mean
+
+########################################## TRI Components for each Respondent ##########################################################
 
 
 
-tri_comp[, mean = ]
+
+# Clean Environment
+rm(list = ls())
 
