2c102ef32ee4c1c81c862d4bf207598f9fac4493
Dennis Henning <>
2022-01-14 19:33:44 +0100

    Adding Cluster Validation with demographics data

--- 06_Cluster Analysis/03_Cluster_Validation.R
+++ 06_Cluster Analysis/03_Cluster_Validation.R
@@ -37,15 +37,37 @@ quest_tri_extended <- quest_tri_extended[complete.cases(quest_tri_extended)]
 quest_tri_extended <- cbind(quest_tri_extended, tri_comp_all_noNA[, "Predicted Class (4Cs)"])
 setnames(quest_tri_extended, "Predicted Class (4Cs)", "Cluster")
 
+# Setting names
+quest_tri_extended$Cluster <- as.character(quest_tri_extended$Cluster)
+quest_tri_extended[ Cluster == "3", Cluster := "Explorers"]
+quest_tri_extended[ Cluster == "4", Cluster := "Hesitators"]
+quest_tri_extended[ Cluster == "2", Cluster := "Avoiders"]
+quest_tri_extended[ Cluster == "1", Cluster := "Pioneers"]
+
+# Setting Cluster as Factor and Levels corresponding Overall TRI
+quest_tri_extended$Cluster <- as.factor(quest_tri_extended$Cluster)
+quest_tri_extended$Cluster <- factor(quest_tri_extended$Cluster , 
+                                     levels = c("Avoiders", "Hesitators", "Pioneers", "Explorers"))
+
+summary(quest_tri_extended$Cluster)
 ########################################## Validation of Clusters ##########################################################
-
-#### Demographic Data ####
-# Age Distribution, Gender, Mean Household Income per Month, Level of Education
-
 quest_tri_extended
 tri_segments_4C_summary
 
+table(quest_tri_extended$v_169)
 
+#### Demographic Data ####
+# Gender, Age, Mean Household Income per Month, Level of Education
+# Gender: v_169 -> Male = 1, Female = 2
+f_c <- quest_tri_extended[v_169 == 2, .("Female (N)" = .N ), by = "Cluster"]
+n_c <- quest_tri_extended[, .(N = .N), by = "Cluster"]
+
+demo <- cbind(f_c, n_c)
+demo[, 3 := NULL]
+demo[, "Female (%)" := round(`Female (N)` / `N`,2)][, `Female (N)` := NULL]
+demo
+
+# Age: v_285
 
 # Clean Environment
 rm(list = ls())
